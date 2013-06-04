33d5c190e680469fca48b63ebc621f4f71487b66
Manuel <manuel.bareiss92@gmail.com>
2013-06-04 12:28:21 +0200

    Post-Style

--- src/header.php
+++ src/header.php
@@ -11,8 +11,8 @@
         session_start();
         if (!isset($_SESSION['username'])) {
             echo"<form action=\"insert.php\" method=\"post\">
-                    <input type=\"text\" name=\"username\">
-                    <input type=\"password\" name=\"pw\" >
+                    <input type=\"text\" name=\"username\" placeholder='Benutzername'>
+                    <input type=\"password\" name=\"pw\" placeholder='Passwort'>
                     <input type=\"hidden\" name=\"mode\" value=\"login\">
                     <input type=\"submit\" value=\"Login\">
                 </form>";
--- src/showthread.php
+++ src/showthread.php
@@ -3,27 +3,41 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
+    
 <?php
 session_start();
 include 'connection.php';
+include 'header.php';
 $id_get = $_GET["t"];
 
-$sql = "SELECT Posts.ID, Posts.Title, Users.Name, Posts.Content FROM Posts, Users WHERE Thread = $id_get AND Users.ID = Posts.User";
+$sqlt = "SELECT * FROM Threads WHERE ID = $id_get";
+$result = mysql_query($sqlt);
+$rowt = mysql_fetch_array($result);
+
+echo "<div class='ThreadName'>".$rowt['Name']."</div><div class='ThreadDate'>".$rowt['Erstellungsdatum']."</div>
+    <div class='ThreadDescr'>".$rowt['Beschreibung']."</div>";
+
+
+$sql = "SELECT Posts.ID, Posts.Title, Users.Name, Posts.Content, Posts.Date FROM Posts, Users WHERE Thread = $id_get AND Users.ID = Posts.User";
 $result = mysql_query($sql);
 echo mysql_error();
 while ($row = mysql_fetch_object($result)) {
     echo "<div class ='Post'>
-              <div class='PostTitle'>
-                <a href='showpost.php?p=$row->ID'>$row->ID</a>$row->Title
+              <div class='wrapTitleDate'>
+                  <div class='PostTitle'>
+                    <a href='showpost.php?p=$row->ID'>#$row->ID</a> $row->Title
+                  </div>
+                  <div class='PostDate'>$row->Date</div>
               </div>
-              <div class='PostUser'>
-                $row->Name
-              </div>
-              <div class='PostContent'>
-                $row->Content
+              <div class='wrapPostUserContent'>
+                  <div class='PostUser'>
+                    $row->Name
+                  </div>
+                  <div class='PostContent'>
+                    $row->Content
+                  </div>
               </div>
           </div>";
-    echo "<hr>";
 }
 ?>
         <form action="insert.php" method="post">
